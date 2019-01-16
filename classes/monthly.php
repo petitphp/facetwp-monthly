358a027ec3871e875f951eed7e2599a1e6724d39
Nestor Vera <nestorvera@me.com>
2019-01-16 13:37:30 +0100

    Fix settings not showing up inside admin

--- classes/monthly.php
+++ classes/monthly.php
@@ -33,12 +33,12 @@ class FacetWP_Facet_Monthly {
 		$where_clause = apply_filters( 'facetwp_facet_where', $where_clause, $facet );
 
 		$sql = "
-        SELECT DATE_FORMAT(f.facet_value, '%Y-%m') as facet_value, f.facet_display_value, COUNT(*) AS counter
-        FROM {$wpdb->prefix}facetwp_index f
-        WHERE f.facet_name = '{$facet['name']}' $where_clause
-        GROUP BY DATE_FORMAT(f.facet_value, '%Y-%m')
-        ORDER BY $orderby
-        LIMIT $limit";
+		SELECT DATE_FORMAT(f.facet_value, '%Y-%m') as facet_value, f.facet_display_value, COUNT(*) AS counter
+		FROM {$wpdb->prefix}facetwp_index f
+		WHERE f.facet_name = '{$facet['name']}' $where_clause
+		GROUP BY DATE_FORMAT(f.facet_value, '%Y-%m')
+		ORDER BY $orderby
+		LIMIT $limit";
 
 		return $wpdb->get_results( $sql, ARRAY_A );
 	}
@@ -53,8 +53,7 @@ class FacetWP_Facet_Monthly {
 		$values          = (array) $params['values'];
 		$selected_values = (array) $params['selected_values'];
 
-		$label_any = empty( $facet['label_any'] ) ? __( 'Any', 'fwp' ) : sprintf( __( '%s', 'fwp' ),
-			$facet['label_any'] );
+		$label_any = empty( $facet['label_any'] ) ? __( 'Any', 'fwp' ) : sprintf( __( '%s', 'fwp' ), $facet['label_any'] );
 
 		$output .= '<select class="facetwp-monthly">';
 		$output .= sprintf( '<option value="">%s</option>', esc_html( $label_any ) );
@@ -69,10 +68,12 @@ class FacetWP_Facet_Monthly {
 				$display_value .= sprintf( ' (%s)', $result['counter'] );
 			}
 
-			$output .= sprintf( '<option value="%s" %s>%s</option>',
+			$output .= sprintf(
+				'<option value="%s" %s>%s</option>',
 				esc_attr( $result['facet_value'] ),
 				$selected,
-				esc_html( $display_value ) );
+				esc_html( $display_value )
+			);
 		}
 
 		$output .= '</select>';
@@ -95,15 +96,15 @@ class FacetWP_Facet_Monthly {
 		$dates = explode( '-', reset( $selected_values ) );
 
 		if ( count( $dates ) < 2 ) {
-			$dates = array( date( 'Y' ), date( 'm' )  );
+			$dates = array( date( 'Y' ), date( 'm' ) );
 		}
 
 		$sql = $wpdb->prepare( "SELECT DISTINCT post_id
-            FROM {$wpdb->prefix}facetwp_index
-            WHERE facet_name = %s
-            AND YEAR(facet_value) = %d
-            AND MONTH(facet_value) = %d
-            ORDER BY facet_value DESC",
+			FROM {$wpdb->prefix}facetwp_index
+			WHERE facet_name = %s
+			AND YEAR(facet_value) = %d
+			AND MONTH(facet_value) = %d
+			ORDER BY facet_value DESC",
 			$facet['name'],
 			absint( $dates[0] ),
 			absint( $dates[1] )
@@ -118,95 +119,94 @@ class FacetWP_Facet_Monthly {
 	 * Output admin settings HTML
 	 */
 	function settings_html() { ?>
-        <tr class="facetwp-conditional type-monthly">
-            <td>
+		<div class="facetwp-row">
+			<div>
 				<?php _e( 'Default label', 'fwp' ); ?>:
-                <div class="facetwp-tooltip">
-                    <span class="icon-question">?</span>
-                    <div class="facetwp-tooltip-content">
-                        Customize the first option label (default: "Any")
-                    </div>
-                </div>
-            </td>
-            <td>
-                <input type="text" class="facet-label-any" value="<?php _e( 'Any', 'fwp' ); ?>"/>
-            </td>
-        </tr>
-        <tr class="facetwp-conditional type-monthly">
-            <td>
+				<div class="facetwp-tooltip">
+					<span class="icon-question">?</span>
+					<div class="facetwp-tooltip-content">
+						<?php _e( 'Customize the first option label (default: "Any")', 'fwp' ); ?>
+					</div>
+				</div>
+			</div>
+			<div><input type="text" class="facet-label-any" value="<?php _e( 'Any', 'fwp' ); ?>"/></div>
+		</div>
+		<div class="facetwp-row">
+			<div>
 				<?php _e( 'Archive order', 'fwp' ); ?>:
-                <div class="facetwp-tooltip">
-                    <span class="icon-question">?</span>
-                    <div class="facetwp-tooltip-content">
-                        Customize the archives order (default: "Newest to Oldest")
-                    </div>
-                </div>
-            </td>
-            <td>
-                <select class="facet-orderby">
-                    <option value="desc" selected><?php _e( 'Newest to Oldest', 'fwp' ); ?></option>
-                    <option value="asc"><?php _e( 'Oldest to newest', 'fwp' ); ?></option>
-                </select>
-            </td>
-        </tr>
-        <tr class="facetwp-conditional type-monthly">
-            <td>
+				<div class="facetwp-tooltip">
+					<span class="icon-question">?</span>
+					<div class="facetwp-tooltip-content">
+						<?php _e( 'Customize the archives order (default: "Newest to Oldest")', 'fwp' ); ?>
+					</div>
+				</div>
+			</div>
+			<div>
+				<select class="facet-orderby">
+					<option value="desc" selected><?php _e( 'Newest to Oldest', 'fwp' ); ?></option>
+					<option value="asc"><?php _e( 'Oldest to newest', 'fwp' ); ?></option>
+				</select>
+			</div>
+		</div>
+		<div class="facetwp-row">
+			<div>
 				<?php _e( 'Count', 'fwp' ); ?>:
-                <div class="facetwp-tooltip">
-                    <span class="icon-question">?</span>
-                    <div class="facetwp-tooltip-content"><?php _e( 'The maximum number of facet choices to show',
-							'fwp' ); ?></div>
-                </div>
-            </td>
-            <td><input type="text" class="facet-count" value="10"/></td>
-        </tr>
+				<div class="facetwp-tooltip">
+					<span class="icon-question">?</span>
+					<div class="facetwp-tooltip-content">
+						<?php _e( 'The maximum number of facet choices to show', 'fwp' ); ?>
+					</div>
+				</div>
+			</div>
+			<div><input type="text" class="facet-count" value="10"/></div>
+		</div>
 	<?php }
 
 	/**
 	 * Output any admin scripts
 	 */
 	function admin_scripts() { ?>
-        <script>
-          (function ($) {
-            wp.hooks.addAction('facetwp/load/monthly', function ($this, obj) {
-              $this.find('.facet-source').val(obj['source']);
-              $this.find('.type-monthly .facet-label-any').val(obj['label_any']);
-              $this.find('.type-monthly .facet-orderby').val(obj['orderby']);
-              $this.find('.type-monthly .facet-count').val(obj['count']);
-            });
+		<script>
+			(function ($) {
+				wp.hooks.addAction('facetwp/load/monthly', function ($this, obj) {
+				$this.find('.facet-source').val(obj['source']);
+				$this.find('.type-monthly .facet-label-any').val(obj['label_any']);
+				$this.find('.type-monthly .facet-orderby').val(obj['orderby']);
+				$this.find('.type-monthly .facet-count').val(obj['count']);
+				});
 
-            wp.hooks.addFilter('facetwp/save/monthly', function ($this, obj) {
-              obj['source'] = $this.find('.facet-source').val();
-              obj['label_any'] = $this.find('.type-monthly .facet-label-any').val();
-              obj['orderby'] = $this.find('.type-monthly .facet-orderby').val();
-              obj['count'] = $this.find('.type-monthly .facet-count').val();
-              return obj;
-            });
-          })(jQuery);
-        </script>
+				wp.hooks.addFilter('facetwp/save/monthly', function ($this, obj) {
+				obj['source'] = $this.find('.facet-source').val();
+				obj['label_any'] = $this.find('.type-monthly .facet-label-any').val();
+				obj['orderby'] = $this.find('.type-monthly .facet-orderby').val();
+				obj['count'] = $this.find('.type-monthly .facet-count').val();
+				return obj;
+				});
+			})(jQuery);
+		</script>
 	<?php }
 
 	/**
 	 * Output any front-end scripts
 	 */
 	function front_scripts() { ?>
-        <script>
-          (function ($) {
-            wp.hooks.addAction('facetwp/refresh/monthly', function ($this, facet_name) {
-              var val = $this.find('.facetwp-monthly').val();
-              FWP.facets[facet_name] = val ? [val] : [];
-            });
+		<script>
+			(function ($) {
+				wp.hooks.addAction('facetwp/refresh/monthly', function ($this, facet_name) {
+				var val = $this.find('.facetwp-monthly').val();
+				FWP.facets[facet_name] = val ? [val] : [];
+				});
 
-            wp.hooks.addAction('facetwp/ready', function () {
-              $(document).on('change', '.facetwp-facet .facetwp-monthly', function () {
-                var $facet = $(this).closest('.facetwp-facet');
-                if ('' != $facet.find(':selected').val()) {
-                  FWP.static_facet = $facet.attr('data-name');
-                }
-                FWP.autoload();
-              });
-            });
-          })(jQuery);
-        </script>
+				wp.hooks.addAction('facetwp/ready', function () {
+				$(document).on('change', '.facetwp-facet .facetwp-monthly', function () {
+					var $facet = $(this).closest('.facetwp-facet');
+					if ('' != $facet.find(':selected').val()) {
+					FWP.static_facet = $facet.attr('data-name');
+					}
+					FWP.autoload();
+				});
+				});
+			})(jQuery);
+		</script>
 	<?php }
 }
